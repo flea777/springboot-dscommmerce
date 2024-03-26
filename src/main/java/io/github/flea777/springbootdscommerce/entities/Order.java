001d166fb078ad789cae4491ac39e683ec5f3e1e
Pedro Bim <pedrodinizz2@outlook.com>
2024-03-25 22:29:25 -0300

    Payment

--- src/main/java/io/github/flea777/springbootdscommerce/entities/Order.java
+++ src/main/java/io/github/flea777/springbootdscommerce/entities/Order.java
@@ -20,14 +20,18 @@ public class Order {
     @JoinColumn(name = "client_id")
     private User client;
 
+    @OneToOne(mappedBy = "order", cascade = CascadeType.ALL)
+    private Payment payment;
+
     public Order() {
     }
 
-    public Order(Long id, Instant moment, OrderStatus status, User client) {
+    public Order(Long id, Instant moment, OrderStatus status, User client, Payment payment) {
         this.id = id;
         this.moment = moment;
         this.status = status;
         this.client = client;
+        this.payment = payment;
     }
 
     public Long getId() {
@@ -60,5 +64,13 @@ public class Order {
 
     public void setClient(User client) {
         this.client = client;
+    }
+
+    public Payment getPayment() {
+        return payment;
+    }
+
+    public void setPayment(Payment payment) {
+        this.payment = payment;
     }
 }
