b32479e831414edca2228750d9c5ac621ff91cd8
Pedro Bim <pedrodinizz2@outlook.com>
2024-04-03 17:22:06 -0300

    ProductController

---
+++ src/main/java/io/github/flea777/springbootdscommerce/controllers/ProductController.java
@@ -0,0 +1,15 @@
+package io.github.flea777.springbootdscommerce.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/products")
+public class ProductController {
+
+    @GetMapping
+    public String test() {
+        return "Hello world!";
+    }
+}
