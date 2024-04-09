60cc3603efc534c08a036a65a2fdc594cd9e36be
Pedro Bim <pedrodinizz2@outlook.com>
2024-04-09 04:03:29 -0300

    PUT - update product

--- src/main/java/io/github/flea777/springbootdscommerce/controllers/ProductController.java
+++ src/main/java/io/github/flea777/springbootdscommerce/controllers/ProductController.java
@@ -37,4 +37,10 @@ public class ProductController {
                 buildAndExpand(dto.getId()).toUri();
         return ResponseEntity.created(uri).body(dto);
     }
+
+    @PutMapping(value = "/{id}")
+    public ResponseEntity<ProductDTO> update(@PathVariable Long id, @RequestBody ProductDTO dto) {
+        dto = service.update(id, dto);
+        return ResponseEntity.ok(dto);
+    }
 }
