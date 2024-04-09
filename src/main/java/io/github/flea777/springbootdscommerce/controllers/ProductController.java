5e5a62c38b6cef714ba701824f3735b14b5fd54d
Pedro Bim <pedrodinizz2@outlook.com>
2024-04-09 03:23:44 -0300

    Get methods with pageable

--- src/main/java/io/github/flea777/springbootdscommerce/controllers/ProductController.java
+++ src/main/java/io/github/flea777/springbootdscommerce/controllers/ProductController.java
@@ -1,14 +1,14 @@
 package io.github.flea777.springbootdscommerce.controllers;
 
-import io.github.flea777.springbootdscommerce.dtos.ProductDTO;A
+import io.github.flea777.springbootdscommerce.dtos.ProductDTO;
 import io.github.flea777.springbootdscommerce.services.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.Optional;
 
 @RestController
 @RequestMapping(value = "/products")
@@ -20,5 +20,10 @@ public class ProductController {
     @GetMapping(value = "/{id}")
     public ProductDTO findById(@PathVariable Long id) {
         return service.findById(id);
+    }
+
+    @GetMapping()
+    public Page<ProductDTO> findAll(Pageable pageable) {
+        return service.findAll(pageable);
     }
 }
--- src/main/java/io/github/flea777/springbootdscommerce/services/ProductService.java
+++ src/main/java/io/github/flea777/springbootdscommerce/services/ProductService.java
@@ -4,10 +4,10 @@ import io.github.flea777.springbootdscommerce.dtos.ProductDTO;
 import io.github.flea777.springbootdscommerce.entities.Product;
 import io.github.flea777.springbootdscommerce.repositories.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-
-import java.util.Optional;
 
 @Service
 public class ProductService {
@@ -19,5 +19,11 @@ public class ProductService {
     public ProductDTO findById(Long id) {
         Product product = repository.findById(id).get();
         return new ProductDTO(product);
+    }
+
+    @Transactional(readOnly = true)
+    public Page<ProductDTO> findAll(Pageable pageable) {
+        Page<Product> list = repository.findAll(pageable);
+        return list.map(x -> new ProductDTO(x));
     }
 }
