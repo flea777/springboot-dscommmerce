aa55d5f5043e09ee73a798e73df9a23d0386ea89
Pedro Bim <pedrodinizz2@outlook.com>
2024-05-02 20:16:28 -0300

    Customized exception in json body

---
+++ src/main/java/io/github/flea777/springbootdscommerce/controllers/handlers/ControllerExceptionHandler.java
@@ -0,0 +1,22 @@
+package io.github.flea777.springbootdscommerce.controllers.handlers;
+
+import io.github.flea777.springbootdscommerce.dtos.CustomError;
+import io.github.flea777.springbootdscommerce.services.exceptions.ResourceNotFoundException;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.time.Instant;
+
+@ControllerAdvice
+public class ControllerExceptionHandler {
+
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public ResponseEntity<CustomError> resourceNotFound(ResourceNotFoundException e, HttpServletRequest request) {
+        HttpStatus status = HttpStatus.NOT_FOUND;
+        var err = new CustomError(Instant.now(), status.value(), e.getMessage(), request.getRequestURI());
+        return ResponseEntity.status(status).body(err);
+    }
+}
